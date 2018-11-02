c1840a77fcea64b69030d85464cb8722e7d59134
SravaniSettipalli <44672598+SravaniSettipalli@users.noreply.github.com>
2018-11-02 18:44:40 +0530

    Add files via upload

---
+++ Contactsdto.java
@@ -0,0 +1,26 @@
+package com.agilecrm.dto;
+
+public class Contactsdto {
+	String name;
+	String email;
+	int mobile;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getMobile() {
+		return mobile;
+	}
+	public void setMobile(int mobile) {
+		this.mobile = mobile;
+	}
+	
+}
